5262192fc9a57926ddc629560e33e61cb226e753
cotowv <cotowv@gmail.com>
2026-09-13 22:40:52 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'cochamo-v12';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './vendor/jspdf.umd.min.js',
+  './logo.jpeg',
+  './icon.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('message', e => {
+  if(e.data === 'skipWaiting') self.skipWaiting();
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  const isHTML = req.mode === 'navigate' || (req.headers.get('accept')||'').includes('text/html');
+  if(isHTML){
+    e.respondWith(
+      fetch(req).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put('./index.html', copy)).catch(()=>{});
+        return resp;
+      }).catch(()=> caches.match('./index.html'))
+    );
+  } else {
+    e.respondWith(
+      caches.match(req).then(r => r || fetch(req).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => { try{ c.put(req, copy); }catch(_){} });
+        return resp;
+      }).catch(()=>caches.match('./index.html')))
+    );
+  }
+});
